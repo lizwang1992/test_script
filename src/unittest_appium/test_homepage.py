5124fc27b0152fffcade1ef3d845179a19c71f08
LizWang <lizwang1992@163.com>
2017-08-27 18:10:18 +0800

    增加标签编辑、我的应用编辑用例

--- src/unittest_appium/test_homepage.py
+++ src/unittest_appium/test_homepage.py
@@ -6,6 +6,7 @@ Created on 2017年8月10日
 import unittest
 from appium import webdriver
 import time
+from selenium.common.exceptions import NoSuchElementException
 
 class homepage_test(unittest.TestCase):
     
@@ -28,9 +29,20 @@ class homepage_test(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
-        print('----------Test over----------')
-        
+        print('--------Test Over--------')
+     
+    def tearDown(self):
+        # 返回首页
+        while True: 
+            try:
+                back_button = self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_left')
+                back_button.click()
+                time.sleep(1)
+            except NoSuchElementException:
+                break
+               
     def test_search(self):
+        # 首页搜索功能
         # 点击首页搜索框，进入搜索页面
         self.driver.find_element_by_id('com.cmcc.andedu_phone:id/main_search').click()
         time.sleep(2)
@@ -46,5 +58,68 @@ class homepage_test(unittest.TestCase):
         # self.driver.find_element_by_name('新闻资讯')
         # print(self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_middle').get_attribute('name'))
         self.assertEqual(self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_middle').get_attribute('name'), '新闻资讯', None)
+
+    def test_tagEdit(self):
+        # 首页标签编辑
+        # 滑动至找到“更多”编辑标签
+        x = self.driver.get_window_size()['width']
+        y = self.driver.get_window_size()['height']
+        while True:
+            try:
+                edit_button = self.driver.find_element_by_name('更多')
+                edit_button.click()
+                break
+            except NoSuchElementException:
+                self.driver.swipe(int(x * 0.8), int(y * 0.36), int(x * 0.3), int(y * 0.36))
+        time.sleep(3)
+        # android端需点击标签才能显示删除按钮
+        self.driver.find_element_by_id('com.cmcc.andedu_phone:id/tag_bg').click()
+        # 找到删除第一个能删除的标签，删除并记录
+        selected_tags = self.driver.find_element_by_id('com.cmcc.andedu_phone:id/recycler_view')
+        tags = selected_tags.find_elements_by_id('com.cmcc.andedu_phone:id/tag_bg')
+        '''for t in tags:
+            try:
+                t.find_element_by_id('com.cmcc.andedu_phone:id/tagview_delete')
+                first_tag = t.find_element_by_id('com.cmcc.andedu_phone:id/tagview_title').get_attribute('name')
+                t.find_element_by_id('com.cmcc.andedu_phone:id/tagview_delete').click()
+                break
+            except NoSuchElementException:
+                first_tag = ''
+        print(first_tag)'''
+        t = tags[0]
+        first_tag = t.find_element_by_id('com.cmcc.andedu_phone:id/tagview_title').get_attribute('name')
+        if '作业' == first_tag:
+            t = tags[1]
+            first_tag = t.find_element_by_id('com.cmcc.andedu_phone:id/tagview_title').get_attribute('name')
+        # print(first_tag)
+        t.find_element_by_id('com.cmcc.andedu_phone:id/tagview_delete').click()
+        # 找到刚删除的标签,添加回去
+        if '' != first_tag:
+            try:
+                edit_button = self.driver.find_element_by_name(first_tag)
+                edit_button.click()
+            except NoSuchElementException:
+                self.driver.swipe(int(x * 0.5), int(y * 0.8), int(x * 0.5), int(y * 0.25))
+                edit_button = self.driver.find_element_by_name(first_tag)
+                edit_button.click()
+                self.driver.swipe(int(x * 0.5), int(y * 0.25), int(x * 0.5), int(y * 0.8))
+        # 保存设置，查看标签是否添加
+        self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_right').click()
+        time.sleep(5)
+        self.driver.find_element_by_name(first_tag)
+    
+    def test_myappEdit(self):
+        # 我的应用编辑
+        self.driver.find_element_by_id('com.cmcc.andedu_phone:id/myapp_layout').click()
+        time.sleep(2)
+        self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_right').click()
+        del_app = self.driver.find_element_by_id('com.cmcc.andedu_phone:id/item_text').get_attribute('name')
+        self.driver.find_element_by_id('com.cmcc.andedu_phone:id/check_image').click()
+        self.driver.find_element_by_id('com.cmcc.andedu_phone:id/tv_ok').click()
         self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_left').click()
         self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_left').click()
+        # 多按一次返回才能返回首页
+        self.driver.find_element_by_id('com.cmcc.andedu_phone:id/common_title_left').click()
+        # 查看是否删除成功
+        first_app = self.driver.find_element_by_id('com.cmcc.andedu_phone:id/app_title').get_attribute('name')
+        self.assertNotEqual(del_app, first_app, None)
